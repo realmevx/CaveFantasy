c38311695dbac1dd8a6c8a189035d0b39f097197
realmevx <realmevx@gmail.com>
2020-12-25 01:59:04 +0100

    Create WorldGen.cs

---
+++ WorldGen.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace WorldGen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            int y;
+
+            int[] arrayX = new int[] { 0, 1 };
+            int[] arrayY = new int[] { 0, 3 };
+
+            
+
+          
+
+            //for(i = 0;)
+
+            for (int i = 0; i < arrayX.Length; i++)
+            {
+                
+ 
+
+
+            }
+            
+
+
+            
+        }
+
+        void Rand()
+        {
+
+            Console.WriteLine("Hello World!");
+
+        }
+
+
+
+    }
+}
